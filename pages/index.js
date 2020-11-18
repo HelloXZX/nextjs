0ec33a30d25e4b1a1de0262a6b996cf80cac28a3
play.js <git@gituser.com>
2020-11-18 10:08:54 +0800

    修改首页by xzx

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Say Hello World by XZX</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Say Hello World by XZX
         </h1>
 
         <p className={styles.description}>
